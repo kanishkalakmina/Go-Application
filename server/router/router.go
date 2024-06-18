5920b0f5f9ec0a622d7bd2f8db4513b4ca77089a
Denidu <denidu.20210404@iit.ac.lk>
2024-06-18 17:51:25 +0530

    Backend

--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/neerajbg/go-fiber-blog/controller"
+)
+
+func SetupRoutes(app *fiber.App) {
+
+	app.Get("/", controller.List)
+	app.Get("/:id", controller.Detail)
+	app.Post("/", controller.Create)
+	app.Put("/:id", controller.Update)
+	app.Delete("/:id", controller.Delete)
+
+}
